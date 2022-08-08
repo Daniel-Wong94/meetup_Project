d795b8136062fb49bebdd17aba59e5940cca19e8
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-08-08 10:26:08 -0400

    Added and tested api router

--- meet-up/backend/routes/index.js
+++ meet-up/backend/routes/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const apiRouter = require("./api");
 
 // Test Router
 // router.get("/hello/world", (req, res) => {
 //   res.cookie("XSRF-TOKEN", req.csrfToken());
 //   res.send("Hello World! - Testing Router Works");
 // });
+
+router.use("/api", apiRouter);
 
 // XSRF-Token Reset - not for production
 router.get("/api/csrf/restore", (req, res) => {
