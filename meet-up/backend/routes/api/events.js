f29822a15563bda29889295af2a18181eb3e3a24
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-08-16 09:23:43 -0400

    Add GET /api/events/:eventId

--- meet-up/backend/routes/api/events.js
+++ meet-up/backend/routes/api/events.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const { Event, Group, Venue } = require("../../db/models");
+const { Event, Group, Venue, Image } = require("../../db/models");
+
+// Get details of event by id: GET /api/events/:eventId
+router.get("/:eventId", async (req, res, next) => {
+  const { eventId } = req.params;
+  const event = await Event.findByPk(eventId, {
+    include: [
+      { model: Group, attributes: ["id", "name", "private", "city", "state"] },
+      {
+        model: Venue,
+        attributes: { exclude: ["createdAt", "updatedAt", "groupId"] },
+      },
+      { model: Image, attributes: ["id", "imageableId", "url"] },
+    ],
+  });
+
+  // middleware for no event found
+  if (!event) {
+    const err = new Error("Event couldn't be found");
+    err.status = 404;
+    next(err);
+  }
+
+  res.json(event);
+});
 
 // Get all events: GET /api/events
 router.get("/", async (req, res, next) => {
