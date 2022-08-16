c23150bc6f41c55e7c62f2883b51a152a70f5648
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-08-15 23:33:35 -0400

    Add PATCH /venues/:venue endpoint

--- meet-up/backend/routes/api/index.js
+++ meet-up/backend/routes/api/index.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 const { restoreUser } = require("../../utils/auth.js");
 const usersRouter = require("./users.js");
 const groupsRouter = require("./groups.js");
+const venuesRouter = require("./venues.js");
 
 router.use(restoreUser);
 
 router.use("/users", usersRouter);
 router.use("/groups", groupsRouter);
+router.use("/venues", venuesRouter);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
---
+++ meet-up/backend/routes/api/venues.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const { Venue, Group, Membership } = require("../../db/models");
+const { validateGroup, validateVenue } = require("../../utils/validation.js");
+const { requireAuth } = require("../../utils/auth");
+
+// Edit a venue by id: PATCH venues/:venueId
+router.patch(
+  "/:venueId",
+  requireAuth,
+  validateVenue,
+  async (req, res, next) => {
+    const { user } = req;
+    const { venueId } = req.params;
+    const { address, city, state, lat, lng } = req.body;
+
+    const venue = await Venue.findByPk(venueId);
+    if (!venue) {
+      const err = new Error("Venue couldn't be found");
+      err.status = 400;
+      next(err);
+    }
+
+    // authorization middleware here:
+    const group = await venue.getGroup();
+    if (!group) {
+      const err = new Error("Group couldn't be found");
+      err.status = 404;
+      next(err);
+    }
+
+    const membership = await Membership.findOne({
+      where: { memberId: user.id, groupId: group.id },
+    });
+
+    if (!membership) {
+      const err = new Error("Must be co-host");
+      err.status = 404;
+      next(err);
+    }
+
+    if (group.organizerId === user.id || membership.status === "co-host") {
+      await venue.update({
+        address: address || venue.address,
+        city: city || venue.city,
+        state: state || venue.state,
+        lat: lat || venue.lat,
+        lng: lng || venue.lng,
+      });
+
+      res.json(venue);
+    }
+  }
+);
+
+module.exports = router;
