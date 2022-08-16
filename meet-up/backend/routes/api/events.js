511b6241147d3b176258e1f2c80e4fb34444875d
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-08-16 12:11:57 -0400

    Add POST /api/events/:eventId/images

--- meet-up/backend/routes/api/events.js
+++ meet-up/backend/routes/api/events.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const { Event, Group, Venue, Image } = require("../../db/models");
+const { requireAuth } = require("../../utils/auth");
+
+// Add an image to an event: POST /api/events/:eventId/images
+router.post("/:eventId/images", requireAuth, async (req, res, next) => {
+  try {
+    const { eventId } = req.params;
+    const { url } = req.body;
+    const { id: userId } = req.user;
+
+    const event = await Event.findByPk(eventId);
+    const image = await event.createImage({ url, userId });
+    const { id, imageableId } = image;
+
+    res.json({ id, url, imageableId });
+  } catch (e) {
+    const err = new Error("Event couldn't be found");
+    err.status = 404;
+    next(err);
+  }
+});
 
 // Get details of event by id: GET /api/events/:eventId
 router.get("/:eventId", async (req, res, next) => {
