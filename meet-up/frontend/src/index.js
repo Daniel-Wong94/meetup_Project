97e9e4c15ade1c65a597bb613de53953afb2cf65
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-09-15 19:50:03 -0400

    set up csrf protection to frontend

--- meet-up/frontend/src/index.js
+++ meet-up/frontend/src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
-import './index.css';
-import App from './App';
-import configureStore from './store';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
+  // call restoreCSRF when in development
+  restoreCSRF();
+
   window.store = store;
+  window.csrfFetch = csrfFetch;
 }
 
 function Root() {
@@ -26,5 +31,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
