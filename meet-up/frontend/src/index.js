d7094aa7d6225f95e0859cf33599339b26fca138
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-09-15 20:39:21 -0400

    created login thunk creator and session reducer

--- meet-up/frontend/src/index.js
+++ meet-up/frontend/src/index.js
@@ -6,15 +6,16 @@ import "./index.css";
 import App from "./App";
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
-  // call restoreCSRF when in development
   restoreCSRF();
 
   window.store = store;
   window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
---
+++ meet-up/frontend/src/store/session.js
@@ -0,0 +1,54 @@
+// All actions/reducers specific to session user's information
+import { csrfFetch } from "./csrf";
+
+// Type constants:
+const SET_USER = "session/SET_USER";
+const REMOVE_USER = "session/REMOVE_USER";
+
+// Action creators:
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    user,
+  };
+};
+
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+
+// THUNK creators:
+export const login = (user) => async (dispatch) => {
+  const { email, password } = user;
+  const response = await csrfFetch("/api/users/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+  const data = await response.json();
+  dispatch(setUser(data));
+  return response;
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  const newState = { ...state };
+  switch (action.type) {
+    case SET_USER:
+      newState.user = action.user;
+      return newState;
+    case REMOVE_USER:
+      newState.user = null;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
