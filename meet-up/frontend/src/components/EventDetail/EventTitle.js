e8a773a507ec1f70b074c21306c4bdf10915433a
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-10-03 19:00:11 -0400

    add optional chaining to eventTitle

--- meet-up/frontend/src/components/EventDetail/EventTitle.js
+++ meet-up/frontend/src/components/EventDetail/EventTitle.js
@@ -20,7 +20,7 @@ const EventTitle = ({ event }) => {
       <div className={styles.imageContainer}>
         <img
           src={
-            group.previewImage ||
+            group?.previewImage ||
             "https://theme.zdassets.com/theme_assets/2041222/c3ea09fd3c3bd646257ea97a6083bf5f45807354.png"
           }
           alt={group.name}
